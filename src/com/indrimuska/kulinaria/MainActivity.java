61798352274189184cc60b145fc1680e65b88485
Indri Muska <indrimuska@gmail.com>
2012-05-11 23:18:07 +0200

    Aggiunte le pagine allo slider

--- src/com/indrimuska/kulinaria/MainActivity.java
+++ src/com/indrimuska/kulinaria/MainActivity.java
@@ -1,55 +1,152 @@
 package com.indrimuska.kulinaria;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.viewpagerindicator.PageIndicator;
 import com.viewpagerindicator.TabPageIndicator;
 import com.viewpagerindicator.TitleProvider;
 
 public class MainActivity extends FragmentActivity {
-	private static final String[] indicators = new String[] { "Dispensa", "Menu", "Ricette", "Shopping List" };
-	
 	SliderAdapter sliderAdapter;
 	ViewPager pager;
 	PageIndicator indicator;
+	DatabaseInterface db;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
+		// Initializing slider
 		sliderAdapter = new SliderAdapter(getSupportFragmentManager());
-
+		
 		pager = (ViewPager) findViewById(R.id.pager);
 		pager.setAdapter(sliderAdapter);
 		
 		indicator = (TabPageIndicator) findViewById(R.id.indicator);
 		indicator.setViewPager(pager);
+		
+		// Initializing database
+		db = new DatabaseInterface(this);
 	}
 	
 	class SliderAdapter extends FragmentPagerAdapter implements TitleProvider {
+		ArrayList<Page> pages = null;
+		
 		public SliderAdapter(FragmentManager fragmentManager) {
 			super(fragmentManager);
+			pages = new ArrayList<Page>();
+			pages.add(new InventoryPage());
+			pages.add(new MenuPage());
+			pages.add(new RecipesPage());
+			pages.add(new ShoppingListPage());
 		}
 
 		@Override
 		public Fragment getItem(int position) {
-			return SliderFragment.newInstance(position);
+			return SliderFragment.newInstance(pages.get(position % pages.size()));
 		}
 
 		@Override
 		public int getCount() {
-			return MainActivity.indicators.length;
+			return pages.size();
 		}
 
 		@Override
 		public String getTitle(int position) {
-			return MainActivity.indicators[position % MainActivity.indicators.length].toUpperCase();
+			return pages.get(position % pages.size()).getTitle().toUpperCase();
+		}
+	}
+	
+	// Generic page to implement
+	abstract static class Page {
+		String pageName;
+		static int pageNumber = -1;
+		public Page() { pageNumber++; }
+		public String getTitle() { return pageName; }
+		public int getPageNumber() { return pageNumber; }
+		public abstract View getView(FragmentActivity activity);
+	}
+	class InventoryPage extends Page {
+		public final int pageNumber;
+		
+		public InventoryPage() {
+			pageName = "Inventory";
+			pageNumber = super.getPageNumber();
+		}
+
+		@Override
+		public View getView(FragmentActivity activity) {
+			TextView text = new TextView(activity);
+			text.setGravity(Gravity.CENTER);
+			text.setText(String.valueOf(pageNumber));
+			text.setTextSize(20 * getResources().getDisplayMetrics().density);
+			text.setPadding(20, 20, 20, 20);
+			return text;
+		}
+	}
+	class MenuPage extends Page {
+		public final int pageNumber;
+		
+		public MenuPage() {
+			pageName = "Menu";
+			pageNumber = super.getPageNumber();
+		}
+
+		@Override
+		public View getView(FragmentActivity activity) {
+			TextView text = new TextView(activity);
+			text.setGravity(Gravity.CENTER);
+			text.setText(String.valueOf(pageNumber));
+			text.setTextSize(20 * getResources().getDisplayMetrics().density);
+			text.setPadding(20, 20, 20, 20);
+			return text;
+		}
+	}
+	class RecipesPage extends Page {
+		public final int pageNumber;
+		
+		public RecipesPage() {
+			pageName = "Recipes";
+			pageNumber = super.getPageNumber();
+		}
+
+		@Override
+		public View getView(FragmentActivity activity) {
+			TextView text = new TextView(activity);
+			text.setGravity(Gravity.CENTER);
+			text.setText(String.valueOf(pageNumber));
+			text.setTextSize(20 * getResources().getDisplayMetrics().density);
+			text.setPadding(20, 20, 20, 20);
+			return text;
+		}
+	}
+	class ShoppingListPage extends Page {
+		public final int pageNumber;
+		
+		public ShoppingListPage() {
+			pageName = "Shopping List";
+			pageNumber = super.getPageNumber();
+		}
+
+		@Override
+		public View getView(FragmentActivity activity) {
+			TextView text = new TextView(activity);
+			text.setGravity(Gravity.CENTER);
+			text.setText(String.valueOf(pageNumber));
+			text.setTextSize(20 * getResources().getDisplayMetrics().density);
+			text.setPadding(20, 20, 20, 20);
+			return text;
 		}
 	}
 }
--- src/com/indrimuska/kulinaria/SliderFragment.java
+++ src/com/indrimuska/kulinaria/SliderFragment.java
@@ -1,5 +1,7 @@
 package com.indrimuska.kulinaria;
 
+import com.indrimuska.kulinaria.MainActivity.Page;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
@@ -8,43 +10,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.TextView;
 
 public final class SliderFragment extends Fragment {
-	private static final String KEY_CONTENT = "SliderFragment:Content";
+	private Page page;
 	
-	private String content;
-	
-	public static SliderFragment newInstance(int pageNumber) {
+	public static SliderFragment newInstance(Page page) {
 		SliderFragment fragment = new SliderFragment();
-		
-		// Setting content string
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < 20; i++) {
-			builder.append(pageNumber).append(" ");
-		}
-		builder.deleteCharAt(builder.length() - 1);
-		fragment.content = builder.toString();
-		
+		fragment.page = page;
 		return fragment;
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		if ((savedInstanceState != null) && savedInstanceState.containsKey(KEY_CONTENT)) {
-			content = savedInstanceState.getString(KEY_CONTENT);
-		}
+		//if ((savedInstanceState != null) && savedInstanceState.containsKey(KEY_CONTENT)) {
+		//	content = savedInstanceState.getString(KEY_CONTENT);
+		//}
 		
-		TextView text = new TextView(getActivity());
-		text.setGravity(Gravity.CENTER);
-		text.setText(content);
-		text.setTextSize(20 * getResources().getDisplayMetrics().density);
-		text.setPadding(20, 20, 20, 20);
+		//TextView text = new TextView(getActivity());
+		//text.setGravity(Gravity.CENTER);
+		//text.setText(String.valueOf(pageNumber));
+		//text.setTextSize(20 * getResources().getDisplayMetrics().density);
+		//text.setPadding(20, 20, 20, 20);
 		
 		LinearLayout layout = new LinearLayout(getActivity());
 		layout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 		layout.setGravity(Gravity.CENTER);
-		layout.addView(text);
+		layout.addView(page.getView(getActivity()));
 		
 		return layout;
 	}
@@ -52,6 +43,6 @@ public final class SliderFragment extends Fragment {
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		outState.putString(KEY_CONTENT, content);
+		//outState.putString(KEY_CONTENT, content);
 	}
 }
