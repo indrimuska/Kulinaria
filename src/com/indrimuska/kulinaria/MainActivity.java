4acdd738318a93d56fc740e4a821e13e1b4adae6
Indri Muska <indrimuska@gmail.com>
2012-05-11 03:58:59 +0200

    Aspetto iniziale MainActivity

---
+++ src/com/indrimuska/kulinaria/MainActivity.java
@@ -0,0 +1,24 @@
+package com.indrimuska.kulinaria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+}
